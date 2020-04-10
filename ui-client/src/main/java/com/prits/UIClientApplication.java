c10713a353013d4efa405d936c50d9f82c71f633
Pritesh Patel <3ppatel@gmail.com>
2020-04-10 11:14:57 -0400

    Updated code

--- ui-client/src/main/java/com/prits/UIClientApplication.java
+++ ui-client/src/main/java/com/prits/UIClientApplication.java
@@ -3,10 +3,6 @@ package com.prits;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class UIClientApplication
 {
