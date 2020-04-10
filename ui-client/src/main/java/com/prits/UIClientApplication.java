5743d0cc4fb3b531530bab0f72f8a1ada3efe85f
Pritesh Patel <3ppatel@gmail.com>
2020-04-10 10:59:20 -0400

    first commit

---
+++ ui-client/src/main/java/com/prits/UIClientApplication.java
@@ -0,0 +1,17 @@
+package com.prits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class UIClientApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run( UIClientApplication.class, args);
+    }
+}
